770bd03413ba2bbaef4a5e8cf8f77363afd0317a
Hafiizh Ghulam <hafiizh.ghulam@frisidea.com>
2023-06-24 13:45:35 +0700

    adjustment

--- resources/views/emails/password-reset.blade.php
+++ resources/views/emails/password-reset.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Ester TA Backend</title>
+    <title>UPTD KPSDKP</title>
 </head>
 <body>
 <a href="{{$url}}" class="">Click Here</a>
